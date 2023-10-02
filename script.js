5e1faecaadb96e81e13653c36b91fc8d8b6341c7
Briannyachae1 <nyachaebrian.com@gmail.com>
2023-10-02 14:36:28 +0300

    fourth commit

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.addEventListener('mousemove', (e) => {
   movingText.style.left = mouseX + 'px';
   movingText.style.top = mouseY + 'px';
 });
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // ... (your form data handling code) ...
+
+  // Display the notification
+  const notification = document.getElementById('submit-notification');
+  notification.style.display = 'block';
+
+  // Hide the notification after a few seconds (adjust the timeout as needed)
+  setTimeout(function() {
+      notification.style.display = 'none';
+  }, 3000); // 3000 milliseconds (3 seconds) in this example
+});
